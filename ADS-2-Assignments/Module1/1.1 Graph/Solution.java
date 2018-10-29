399b030f78724f495a660bfccb5a863c8fa4a9c5
khyathimsit <khyathi17@msitprogram.net>
2018-10-30 00:49:21 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/Module1/1.1 Graph/Solution.java
+++ ADS-2-Assignments/Module1/1.1 Graph/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
 					String vertices = sc.nextLine();
 					String[] tokens1 = vertices.split(",");
 					GraphList list = new GraphList(m);
-					for (int i = 0; i < m; i++) {
+					for (int i = 0; i <= m; i++) {
 						list.addVertex(tokens1[i]);
 					}
 					String edges = sc.nextLine();
@@ -32,6 +32,30 @@ public class Solution {
 						return;
 					} else {
 						System.out.println(list.toString());
+					}
+				break;
+
+				case "Matrix":
+					if (m == 0 || n == 0) {
+						System.out.println(m + " vertices, " + n + " edges");
+						System.out.println("No edges");
+					}
+					String vertices1 = sc.nextLine();
+					String[] token1 = vertices1.split(",");
+					AdjMatrixGraph matrix = new AdjMatrixGraph(m);
+					for (int i = 0; i < m; i++) {
+						matrix.addVertex(token1[i]);
+					}
+					String edges1 = sc.nextLine();
+					String[] token2 = new String[100];
+					for (int i = 0; i < n; i++) {
+						token2 = edges1.split(" ");
+						matrix.addEdge(Integer.parseInt(token2[0]), Integer.parseInt(token2[0]));
+					}
+					if (Integer.parseInt(token2[0]) == Integer.parseInt(token2[1])) {
+						break;
+					} else {
+						System.out.println(matrix.toString());
 					}
 				break;
 
