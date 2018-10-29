f001f2639f3c0019b86874374034e1a51c719744
khyathimsit <khyathi17@msitprogram.net>
2018-10-30 01:41:54 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-Assignments/Module1/1.1 Graph/AdjMatrixGraph.java
+++ ADS-2-Assignments/Module1/1.1 Graph/AdjMatrixGraph.java
@@ -1,48 +1,102 @@
-import java.util.Iterator;
+/**.
+ * List of graphs.
+ */
 public class AdjMatrixGraph {
-    private final int V;
-    private int E;
+    /**.
+     * { var_description }
+     */
+    private final int ver;
+    /**.
+     * { var_description }
+     */
+    private int edg;
+    /**.
+     * { var_description }
+     */
     private int[][] adj;
+    /**.
+     * { var_description }
+     */
     private String[] vertexes;
+    /**.
+     * { var_description }
+     */
     private int size = 0;
-    public AdjMatrixGraph(int V) {
-        this.V = V;
-        this.E = 0;
-        this.adj = new int[V][V];
-        vertexes = new String[V];
+    /**.
+     * Constructs the object.
+     *
+     * @param      vt     { parameter_description }
+     */
+    public AdjMatrixGraph(final int vt) {
+        this.ver = vt;
+        this.edg = 0;
+        this.adj = new int[ver][ver];
+        vertexes = new String[ver];
         size = 0;
     }
-    public int V() {
-        return V;
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int vert() {
+        return ver;
     }
-    public int E() {
-        return E;
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int edge() {
+        return edg;
     }
-    public void addVertex(String v) {
+    /**.
+     * Adds a vertex.
+     *
+     * @param      v     { parameter_description }
+     */
+    public void addVertex(final String v) {
         vertexes[size] = v;
         size++;
     }
-    public void addEdge(int v, int w) {
+    /**
+     * .
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    public void addEdge(final int v, final int w) {
         if (v == w) {
-            System.out.println(V + " vertices, " + E + " edges");
+            System.out.println(ver + " vertices, " + edg + " edges");
             System.out.println("No edges");
             return;
         }
         if (adj[v][w] == 0) {
-            E++;
+            edg++;
             adj[v][w] = 1;
             adj[w][v] = 1;
         }
     }
-
-    public int contains(int v, int w) {
+    /**.
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int contains(final int v, final int w) {
         return adj[v][w];
     }
-
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges" + '\n');
-        for (int v = 0; v < V; v++) {
+        s.append(ver + " vertices, " + edg + " edges" + '\n');
+        for (int v = 0; v < ver; v++) {
             // s.append(v + ": ");
             for (int w : adj[v]) {
                 if (w == 1) {
--- ADS-2-Assignments/Module1/1.1 Graph/GraphList.java
+++ ADS-2-Assignments/Module1/1.1 Graph/GraphList.java
@@ -1,65 +1,186 @@
+/**.
+ * Interface for graph.
+ */
 interface Graph {
-    public int V();
-    public int E();
-    public void addEdge(int v, int w);
-    public Iterable<Integer> adj(int v);
-    //public boolean hasEdge(int v, int w);
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int vert();
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int edge();
+    /**.
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    void addEdge(int v, int w);
+    /**.
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    Iterable<Integer> adj(int v);
+    /**.
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    boolean hasEdge(int v, int w);
 }
-
-public class GraphList implements Graph {
-    private final int V;
-    private int E;
+/**.
+ * List of graphs.
+ */
+class GraphList implements Graph {
+    /**.
+     * { var_description }
+     */
+    private int ver;
+    /**.
+     * { var_description }
+     */
+    private int edg;
+    /**.
+     * { var_description }
+     */
     private Bag<Integer>[] adj;
-    private String[] vertices;
-    private int size;
-    public GraphList(int V) {
-        this.V = V;
-        this.E = 0;
-        vertices = new String[V];
-        // System.out.println(V);
+    /**.
+     * { var_description }
+     */
+    private String[] vertexes;
+    /**.
+     * { var_description }
+     */
+    private int size = 0;
+    /**
+     * Initializes an empty graph with V vertices and 0 edges.
+     * param V the number of vertices
+     *
+     * @param  vt number of vertices
+     */
+    GraphList(final int vt) {
+        this.ver = vt;
+        this.edg = 0;
+        vertexes = new String[ver];
         size = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
+        adj = (Bag<Integer>[]) new Bag[ver];
+        for (int v = 0; v < ver; v++) {
             adj[v] = new Bag<Integer>();
         }
     }
-    public int V() {
-        return V;
+
+    /**
+     * Returns the number of vertices in this graph.
+     *
+     * @return the number of vertices in this graph
+     */
+    public int vert() {
+        return ver;
     }
-    public int E() {
-        return E;
+
+    /**
+     * Returns the number of edges in this graph.
+     *
+     * @return the number of edges in this graph
+     */
+    public int edge() {
+        return edg;
     }
-    public void addVertex(String v) {
-        vertices[size] = v;
+    /**.
+     * Adds a vertex.
+     *
+     * @param      v     { parameter_description }
+     */
+    public void addVertex(final String v) {
+        vertexes[size] = v;
         size++;
     }
-    public void addEdge(int v, int w) {
+    /**.
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v, final int w) {
+        for (int i : adj[w]) {
+            if (i == w) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Adds the undirected edge v-w to this graph.
+     *
+     * @param  v one vertex in the edge
+     * @param  w the other vertex in the edge
+     */
+    public void addEdge(final int v, final int w) {
         if (v == w) {
-            System.out.println(V + " vertices, " + E + " edges");
+            System.out.println(ver + " vertices, " + edg + " edges");
             System.out.println("No edges");
             return;
         }
-        E++;
+        if (!hasEdge(v, w)) {
+            edg++;
+        }
         adj[v].add(w);
         adj[w].add(v);
     }
-    public Iterable<Integer> adj(int v) {
+    /**
+     * Returns the vertices adjacent to vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the vertices adjacent to vertex {@code v}, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Integer> adj(final int v) {
         return adj[v];
     }
 
-    public int degree(int v) {
+    /**
+     * Returns the degree of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the degree of vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int degree(final int v) {
         return adj[v].size();
     }
+
+
+    /**
+     * Returns a string representation of this graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by
+     * the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists
+     */
     public String toString() {
         String s = "";
-        s += V + " vertices, " + E + " edges" + '\n';
-        for (int v = 0; v < V; v++) {
-            s += vertices[v] + ": ";
+        s += ver + " vertices, " + edg + " edges" + '\n';
+        for (int v = 0; v < ver; v++) {
+            s += vertexes[v] + ": ";
             for (int w : adj[v]) {
-                s += vertices[w] + " ";
+                s += vertexes[w] + " ";
             }
             s += '\n';
         }
         return s;
     }
+
 }
