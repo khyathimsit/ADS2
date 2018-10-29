37af53079d3ded61f1d6e1d58a758eaabc7d39ae
khyathimsit <khyathi17@msitprogram.net>
2018-10-30 01:33:34 +0530

    "ads2_m1_a1_1: 5/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/Module1/1.1 Graph/Solution.java
+++ ADS-2-Assignments/Module1/1.1 Graph/Solution.java
@@ -1,75 +1,90 @@
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
-public class Solution {
-	Solution() {
-	}
-		public static void main(final String[] args) {
-			// GraphList list = new GraphList();
-			// AdjMatrixGraph matrix = new AdjMatrixGraph();
-			Scanner sc = new Scanner(System.in);
-			String word = sc.nextLine();
-			int m = Integer.parseInt(sc.nextLine());
-			int n = Integer.parseInt(sc.nextLine());
-			String[] tokens2 = new String[10];
-			String[] token2 = new String[10];
-			if ((m < 2) || (n < 2)) {
-				System.out.println(m + " vertices, " + n + " edges");
-				System.out.println("No edges");
-			} else 
-			{
-				switch (word){
-				case "List":
-					// if (m == 0 || n == 0) {
-					// 	System.out.println(m + " vertices, " + n + " edges");
-					// 	System.out.println("No edges");
-					// 	break;
-					// }
-					String vertices = sc.nextLine();
-					String[] tokens1 = vertices.split(",");
-					GraphList list = new GraphList(m);
-					for (int i = 0; i < m; i++) {
-						list.addVertex(tokens1[i]);
-					}
-					while(sc.hasNext()){
-					String edges = sc.nextLine();
-					tokens2 = edges.split(" ");
-					for (int i = 0; i < n; i++) {
-						list.addEdge(Integer.parseInt(tokens2[0]), Integer.parseInt(tokens2[0]));
-					}}
-					if (Integer.parseInt(tokens2[0]) == Integer.parseInt(tokens2[1])) {
-						return;
-					} else {
-						System.out.println(list.toString());
-					}
-				break;
-
-				case "Matrix":
-					// if (m == 0 || n == 0) {
-					// 	System.out.println(m + " vertices, " + n + " edges");
-					// 	System.out.println("No edges");
-					// 	break;
-					// }
-					String vertices1 = sc.nextLine();
-					String[] token1 = vertices1.split(",");
-					AdjMatrixGraph matrix = new AdjMatrixGraph(m);
-					for (int i = 0; i < m; i++) {
-						matrix.addVertex(token1[i]);
-					}
-					while(sc.hasNext()){
-					String edges1 = sc.nextLine();
-					for (int i = 0; i < n; i++) {
-						token2 = edges1.split(" ");
-						matrix.addEdge(Integer.parseInt(token2[0]), Integer.parseInt(token2[0]));
-					}}
-					if (Integer.parseInt(token2[0]) == Integer.parseInt(token2[1])) {
-						break;
-					} else {
-						System.out.println(matrix.toString());
-					}
-				break;
-
-				default:
-				break;
-			}
-		}
-	}
+/**.
+ * { item_description }
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    // time complexity for the main method is N
+    // Because there is one while loop.
+    // while loop iterates until it has next line i.e N times.
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int n =  sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+        switch (word) {
+            case"List":
+                if (n == 0 && m == 0) {
+                    System.out.println(n + " vertices, " + m + " edges");
+                    System.out.println("No edges");
+                    break;
+                }
+                String vtexes = sc.nextLine();
+                String[] tokens1 = vtexes.split(",");
+                GraphList gl = new GraphList(n);
+                for (int i = 0; i < n; i++) {
+                    gl.addVertex(tokens1[i]);
+                }
+                String line1;
+                String[] tokens2 = new String[m];
+                for (int i = 0; i < m; i++) {
+                    line1 = sc.nextLine();
+                    tokens2 = line1.split(" ");
+                        gl.addEdge(Integer.parseInt(tokens2[0]),
+                         Integer.parseInt(tokens2[1]));
+                }
+                if (Integer.parseInt(tokens2[0])
+                 == Integer.parseInt(tokens2[1])) {
+                    break;
+                } else {
+                    System.out.println(gl.toString());
+                }
+                break;
+            case"Matrix":
+                if (n == 0 && m == 0) {
+                    System.out.println(n + " vertices, " + m + " edges");
+                    System.out.println("No edges");
+                    break;
+                }
+                String vtxes = sc.nextLine();
+                String[] tkens = vtxes.split(",");
+                AdjMatrixGraph gm = new AdjMatrixGraph(n);
+                for (int i = 0; i < n; i++) {
+                    gm.addVertex(tkens[i]);
+                }
+                String line;
+                String[] tokens = new String[m];
+                for (int i = 0; i < m; i++) {
+                    line = sc.nextLine();
+                    tokens = line.split(" ");
+                    gm.addEdge(Integer.parseInt(tokens[0]),
+                     Integer.parseInt(tokens[1]));
+                }
+                if (Integer.parseInt(tokens[0])
+                 == Integer.parseInt(tokens[1])) {
+                    break;
+                } else {
+                    System.out.println(gm.toString());
+                }
+                break;
+            default:
+            break;
+        }
+    }
 }
