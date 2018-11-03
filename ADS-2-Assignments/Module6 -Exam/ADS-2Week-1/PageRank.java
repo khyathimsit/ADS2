f51766244239c5ef39e0070b4a48bf005d18063d
khyathimsit <khyathi17@msitprogram.net>
2018-11-03 13:16:50 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-Assignments/Module6 -Exam/ADS-2Week-1/PageRank.java
+++ ADS-2-Assignments/Module6 -Exam/ADS-2Week-1/PageRank.java
@@ -2,47 +2,47 @@
  * Class for page rank.
  */
 class PageRank {
-	/**.
-	 * { var_description }
-	 */
-	private Digraph dig;
-	/**.
-	 * { var_description }
-	 */
-	private double[] prvalue;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      d     { parameter_description }
-	 */
-	PageRank(final Digraph d) {
-		this.dig = d;
-		//int vertices = dig.vert(); 
-		prvalue = new double[dig.vert()];
-		for (int i = 0; i < prvalue.length; i++) {
-			prvalue[i] = 1 / dig.vert();
-		}
-		getPR();
-	}
-	/**.
-	 * Gets the pr.
-	 */
-	public void getPR() {
-		for (int i = 0; i < 1000; i++) {
-			for (int j = 0; j < dig.vert(); j++) {
-				double result = 0.0;
-				for(int k : dig.adj(j)) {
-					result += (prvalue[k]/dig.outdegree(k));
-				}
-				prvalue[j] = result;
-			}
-		}
-	}
+    /**.
+     * { var_description }
+     */
+    private Digraph dig;
+    /**.
+     * { var_description }
+     */
+    private double[] prvalue;
+    /**.
+     * Constructs the object.
+     *
+     * @param      d     { parameter_description }
+     */
+    PageRank(final Digraph d) {
+        this.dig = d;
+        //int vertices = dig.vert(); 
+        prvalue = new double[dig.vert()];
+        for (int i = 0; i < prvalue.length; i++) {
+            prvalue[i] = 1 / dig.vert();
+        }
+        getPR();
+    }
+    /**.
+     * Gets the pr.
+     */
+    public void getPR() {
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < dig.vert(); j++) {
+                double result = 0.0;
+                for(int k : dig.adj(j)) {
+                    result += (prvalue[k]/dig.outdegree(k));
+                }
+                prvalue[j] = result;
+            }
+        }
+    }
 
-	// public String toString() {
-	// 	StringBuilder s = new StringBuilder();
-	// 	for (int i = 0; i < dig.vert(); i++) {
-	// 		System.out.println(i + "-" + getPR(i));
-	// 	}
-	// }
+    // public String toString() {
+    //  StringBuilder s = new StringBuilder();
+    //  for (int i = 0; i < dig.vert(); i++) {
+    //      System.out.println(i + "-" + getPR(i));
+    //  }
+    // }
 }
--- ADS-2-Assignments/Module6 -Exam/ADS-2Week-1/Solution.java
+++ ADS-2-Assignments/Module6 -Exam/ADS-2Week-1/Solution.java
@@ -6,34 +6,33 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	Solution() {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int vertices = sc.nextInt();
-		Digraph dg = new Digraph(vertices);
-		PageRank pr = new PageRank(dg);
-		int edges = 0;
-		while (sc.hasNextLine()) {
-			String[] tokens = sc.nextLine().split(" ");
-			for (int i = 1; i < tokens.length; i++) {
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertices = sc.nextInt();
+        Digraph dg = new Digraph(vertices);
+        PageRank pr = new PageRank(dg);
+        int edges = 0;
+        while (sc.hasNextLine()) {
+            String[] tokens = sc.nextLine().split(" ");
+            for (int i = 1; i < tokens.length; i++) {
                 dg.addEdge(Integer.parseInt(tokens[0]),
                  Integer.parseInt(tokens[i]));
                 int e = dg.indegree(i);
-                edges += e; 
+                edges += e;
             }
-		}
-		//System.out.println(vertices + " vertices," + edges + " edges");
-		System.out.println(dg.toString());
-		System.out.println(pr.toString());
-	}
+        }
+        System.out.println(dg.toString());
+        System.out.println(pr.toString());
+    }
 }
