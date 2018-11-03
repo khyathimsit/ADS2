40440b1f207e03b64d2631e89e7bfa12407e4f0e
khyathimsit <khyathi17@msitprogram.net>
2018-11-03 10:44:54 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-Assignments/Module6 -Exam/ADS-2Week-1/Solution.java
@@ -0,0 +1,32 @@
+
+// class PageRank {
+	
+// }
+
+// class WebSearch {
+
+// }
+
+import java.util.Scanner;
+public class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int vertices = sc.nextInt();
+		Digraph dg = new Digraph(vertices);
+		int edges = 0;
+		while (sc.hasNextLine()) {
+			String[] tokens = sc.nextLine().split(" ");
+			for (int i = 1; i < tokens.length; i++) {
+                dg.addEdge(Integer.parseInt(tokens[0]),
+                 Integer.parseInt(tokens[i]));
+                int e = dg.indegree(i);
+                edges += e; 
+            }
+		}
+		//System.out.println(vertices + " vertices," + edges + " edges");
+		System.out.println(dg.toString());
+	}
+}
