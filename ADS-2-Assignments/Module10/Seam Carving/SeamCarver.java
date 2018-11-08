7f97208a808d8dbd11882a6599bccb2a393f38d5
khyathimsit <khyathi17@msitprogram.net>
2018-11-08 15:57:12 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/Module10/Seam Carving/SeamCarver.java
+++ ADS-2-Assignments/Module10/Seam Carving/SeamCarver.java
@@ -1,4 +1,4 @@
-
+import java.awt.Color;
 public class SeamCarver {
 	private Picture picture;
 	// create a seam carver object based on the given picture
@@ -21,7 +21,17 @@ public class SeamCarver {
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		return 0;
+		if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) {
+			return 1000;
+		}
+		return Math.sqrt(calculatesquare(picture.get(x - 1, y), picture.get(x + 1, y))
+		+ calculatesquare(picture.get(x, y - 1), picture.get(x, y + 1)));
+	}
+	private double calculatesquare(Color c1, Color c2) {
+		double red = c1.getRed() - c2.getRed();
+		double green = c1.getGreen() - c2.getGreen();
+		double blue = c1.getBlue() - c2.getBlue();
+		return red * red + green * green + blue * blue;
 	}
 
 	// // sequence of indices for horizontal seam
@@ -34,7 +44,7 @@ public class SeamCarver {
 	// 	return new int[0];
 	// }
 
-	// // remove horizontal seam from current picture
+	// // remove horizontal seam from current pictureture
 	// public void removeHorizontalSeam(int[] seam) {
 
 	// }
