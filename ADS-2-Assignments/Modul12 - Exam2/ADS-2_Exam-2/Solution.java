a7b390a907e3f7d84b939866bbec2b0818b9062f
khyathimsit <khyathi17@msitprogram.net>
2018-11-10 10:45:02 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-Assignments/Modul12 - Exam2/ADS-2_Exam-2/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public final class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int vertices = sc.nextInt();
+		EdgeWeightedGraph e = new EdgeWeightedGraph(vertices);
+		int edges = sc.nextInt();
+		String[] tokens;
+        int ver = vertices;
+        while (ver > 0) {
+        	tokens = sc.nextLine().split(" ");
+        	e.addEdge(new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2])));
+        }
+		String caseToGo = null;
+		switch (caseToGo) {
+		case "Graph":
+			//Print the Graph Object.
+		    System.out.println(e);
+			break;
+
+		// case "DirectedPaths":
+		// 	// Handle the case of DirectedPaths, where two integers are given.
+		// 	// First is the source and second is the destination.
+		// 	// If the path exists print the distance between them.
+		// 	// Other wise print "No Path Found."
+		// 	break;
+
+		// case "ViaPaths":
+		// 	// Handle the case of ViaPaths, where three integers are given.
+		// 	// First is the source and second is the via is the one where path should pass throuh.
+		// 	// third is the destination.
+		// 	// If the path exists print the distance between them.
+		// 	// Other wise print "No Path Found."
+		// 	break;
+
+		default:
+			break;
+		}
+
+	}
+}
