0a80499bc1cf91addbff936fa2f2d91c0ad4bdb3
khyathimsit <khyathi17@msitprogram.net>
2018-11-10 12:08:26 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/Modul12 - Exam2/ADS-2_Exam-2/Solution.java
+++ ADS-2-Assignments/Modul12 - Exam2/ADS-2_Exam-2/Solution.java
@@ -50,11 +50,11 @@ public final class Solution {
             String[] line = sc.nextLine().split(" ");
             Dijkstra dij = new Dijkstra(e, Integer.parseInt(line[0]));
             double dist = dij.distTo(Integer.parseInt(line[2]));
-            // Dijkstra dij1 = new Dijkstra(e, Integer.parseInt(line[1]));
-            // double dist1 = dij1.distTo(Integer.parseInt(line[2]));
-            // double res = dist + dist1;
+            Dijkstra dij1 = new Dijkstra(e, Integer.parseInt(line[1]));
+            double dist1 = dij1.distTo(Integer.parseInt(line[2]));
+            double res = dist + dist1;
             if (dij.hasPathTo(Integer.parseInt(line[2]))) {
-                System.out.println(dist);
+                System.out.println(res);
             } else {
                 System.out.println("No Path Found.");
             }
