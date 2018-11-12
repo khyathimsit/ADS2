5d4a5b1257677676471a7d850ed1cbbb183f60e8
khyathimsit <khyathi17@msitprogram.net>
2018-11-12 14:15:56 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/Module13/Radix Sorts/Quick3string.java
+++ ADS-2-Assignments/Module13/Radix Sorts/Quick3string.java
@@ -1,31 +1,55 @@
-
+/**.
+ * Class for quick 3 string.
+ */
 public class Quick3string {
-    private static final int CUTOFF =  15;   // cutoff to insertion sort
-
-    // do not instantiate
-    public Quick3string() { } 
-
-    /**  
-     * Rearranges the array of strings in ascending order.
-     *
-     * @param a the array to be sorted
+    /**
+     * cutoff variable
      */
-    public static void sort(String[] a) {
+    private static final int CUTOFF =  15;
+
+    /**
+     * Constructs the object.
+     */
+    public Quick3string() { }
+
+    /**.
+     * { sort function }
+     *
+     * @param      a     { parameter_description }
+     */
+    public static void sort(final String[] a) {
         // StdRandom.shuffle(a);
         sort(a, 0, a.length-1, 0);
         assert isSorted(a);
     }
 
-    // return the dth character of s, -1 if d = length of s
-    private static int charAt(String s, int d) { 
+    /**.
+     * { return the dth character of s, -1 if d = length of s }
+     *
+     * @param      s     { parameter_description }
+     * @param      d     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private static int charAt(final String s, final int d) {
         assert d >= 0 && d <= s.length();
-        if (d == s.length()) return -1;
+        if (d == s.length()) {
+            return -1;
+        }
         return s.charAt(d);
     }
 
 
-    // 3-way string quicksort a[lo..hi] starting at dth character
-    private static void sort(String[] a, int lo, int hi, int d) { 
+    /**
+     * 3-way string quicksort a[lo..hi] starting at dth character.
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     * @param      d     { parameter_description }
+     */
+    private static void sort(final String[] a, final int lo,
+        final int hi, final int d) {
 
         // cutoff to insertion sort for small subarrays
         if (hi <= lo + CUTOFF) {
@@ -38,26 +62,48 @@ public class Quick3string {
         int i = lo + 1;
         while (i <= gt) {
             int t = charAt(a[i], d);
-            if      (t < v) exch(a, lt++, i++);
-            else if (t > v) exch(a, i, gt--);
-            else              i++;
+            if (t < v) {
+                exch(a, lt++, i++);
+            } else if (t > v) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
         }
 
-        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]. 
+        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
         sort(a, lo, lt-1, d);
-        if (v >= 0) sort(a, lt, gt, d+1);
+        if (v >= 0) {
+            sort(a, lt, gt, d+1);
+        }
         sort(a, gt+1, hi, d);
     }
 
-    // sort from a[lo] to a[hi], starting at the dth character
-    private static void insertion(String[] a, int lo, int hi, int d) {
-        for (int i = lo; i <= hi; i++)
-            for (int j = i; j > lo && less(a[j], a[j-1], d); j--)
+    /**
+     * sort from a[lo] to a[hi], starting at the dth character.
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     * @param      d     { parameter_description }
+     */
+    private static void insertion(final String[] a, final int lo,
+        final int hi, final int d) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j-1], d); j--) {
                 exch(a, j, j-1);
+            }
+        }
     }
 
-    // exchange a[i] and a[j]
-    private static void exch(String[] a, int i, int j) {
+    /**
+     * exchange a[i] and a[j].
+     *
+     * @param      a     { parameter_description }
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
+    private static void exch(final String[] a, final int i, final int j) {
         String temp = a[i];
         a[i] = a[j];
         a[j] = temp;
@@ -71,34 +117,65 @@ public class Quick3string {
     // }
 
     // is v less than w, starting at character d
-    private static boolean less(String v, String w, int d) {
+    
+    /**
+     * less function.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     * @param      d     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private static boolean less(final String v, final String w,
+        final int d) {
         assert v.substring(0, d).equals(w.substring(0, d));
         for (int i = d; i < Math.min(v.length(), w.length()); i++) {
-            if (v.charAt(i) < w.charAt(i)) return true;
-            if (v.charAt(i) > w.charAt(i)) return false;
+            if (v.charAt(i) < w.charAt(i)) {
+                return true;
+            }
+            if (v.charAt(i) > w.charAt(i)) {
+                return false;
+            }
         }
         return v.length() < w.length();
     }
 
-    // is the array sorted
-    private static boolean isSorted(String[] a) {
-        for (int i = 1; i < a.length; i++)
-            if (a[i].compareTo(a[i-1]) < 0) return false;
+    /**
+     * Determines if sorted.
+     *
+     * @param      a     { parameter_description }
+     *
+     * @return     True if sorted, False otherwise.
+     */
+    private static boolean isSorted(final String[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (a[i].compareTo(a[i-1]) < 0) {
+                return false;
+            }
+        }
         return true;
     }
-
-    public String toString(String[] array) {
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @param      array  The array
+     *
+     * @return     String representation of the object.
+     */
+    public String toString(final String[] array) {
         String string = "[";
-        for(int i=0;i<array.length-1;i++) {
+        for(int i = 0; i < array.length - 1; i++) {
             string += array[i] + ", ";
         }
-        string += array[array.length-1] + "]";
+        string += array[array.length - 1] + "]";
         return string;
     }
 
 
     // /**
-    //  * Reads in a sequence of fixed-length strings from standard input;
+    //  * Reads in a sequence of fixed-length strings
+    //  * from standard input;
     //  * 3-way radix quicksorts them;
     //  * and prints them to standard output in ascending order.
     //  *
@@ -118,3 +195,4 @@ public class Quick3string {
     //         StdOut.println(a[i]);
     // }
 }
+
