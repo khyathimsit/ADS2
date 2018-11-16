e861fb18fd5a4e314481f192699fe5410f33f467
khyathimsit <khyathi17@msitprogram.net>
2018-11-16 10:40:25 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/Module16/boggle/Solution.java
+++ ADS-2-Assignments/Module16/boggle/Solution.java
@@ -27,11 +27,11 @@ public final class Solution {
         switch (caseType) {
         case "Score":
             String dictionaryName = sc.nextLine();
-            In in = new In(" /Files/ " + dictionaryName);
+            In in = new In("/Files/" + dictionaryName);
             String[] dictionary = in.readAllStrings();
             BoggleSolver solver = new BoggleSolver(dictionary);
             String boardName = sc.nextLine();
-            BoggleBoard board = new BoggleBoard(" /Files/ " + boardName);
+            BoggleBoard board = new BoggleBoard("/Files/" + boardName);
             int score = 0;
             for (String word : solver.getAllValidWords(board)) {
                 score += solver.scoreOf(word);
@@ -41,7 +41,7 @@ public final class Solution {
         default:
             try {
                 dictionaryName = sc.nextLine();
-                in = new In(" /Files/ " + dictionaryName);
+                in = new In("/Files/" + dictionaryName);
                 dictionary = in.readAllStrings();
                 solver = new BoggleSolver(dictionary);
                 board = null;
